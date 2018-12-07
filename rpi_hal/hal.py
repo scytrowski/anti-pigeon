e279e6a9e9f91818c7bd4bb9cecdbb319f813b25
vdrake6 <vdrake6@gmail.com>
2018-12-07 15:40:20 +0100

    RPi HAL

---
+++ rpi_hal/hal.py
@@ -0,0 +1,15 @@
+from anti_pigeon.hal import HAL
+
+
+class RPiHAL(HAL):
+    """
+    Default HAL used in the project
+    """
+    def strain_gauge(self):
+        pass
+
+    def camera(self):
+        pass
+
+    def woodcutter_axe(self):
+        pass
